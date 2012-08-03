48c24cecf54b622b08123739aa1b556fd934742a
Edwin Meyer <edwin@leon.(none)>
2012-08-03 11:13:49 -0500

    Initial commit of Articlesystem-Postgres demo app

---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+class Article < ActiveRecord::Base
+  attr_accessible :article, :title
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArticlesystemPg::Application.config.secret_token = 'e9a7143e5f8a0d8a255cc349d1044a3efbcf948114f515beec2aca08c6e794ae4023fe3ed7ca793c30c7ea2a4b4b055592341b553a992a28ed4575bef4350e6e'
